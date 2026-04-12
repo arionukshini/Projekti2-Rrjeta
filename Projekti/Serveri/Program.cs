27da8ee0b87fd406abf51ef9ecade4a7cf947258
Arion <arionukshini01@gmail.com>
2026-04-13 00:27:11 +0200

    riorganizimi i folders dhe punim ne server

---
+++ Projekti/Serveri/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Serveri
+{
+    static void Main() { 
+        string server_ip = "127.0.0.1";
+        int server_port = 4444;
+
+        UdpClient udp_server = new UdpClient(server_port);
+        Console.WriteLine("Serveri filloi ne " + server_ip + ":" + server_port);
+
+        IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
+
+        while (true) {
+            byte[] data = udp_server.Receive(ref endpoint);
+            string message = Encoding.UTF8.GetString(data);
+
+            Console.WriteLine(endpoint + " " + message);
+        }
+    }
+}
