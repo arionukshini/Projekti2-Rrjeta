f106d33dd9af89964ecf3818a4970d3a679b5420
Arion <arionukshini01@gmail.com>
2026-04-13 14:45:56 +0200

    perfundimi i serverit
    permban pjesen per admin dhe readonly, po ashtu foldering per files to serverit

--- Projekti/Serveri/Program.cs
+++ Projekti/Serveri/Program.cs
@@ -14,11 +14,35 @@ class Serveri
 
         IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 0);
 
+        string server_files = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\ServerFiles");
+
+        if (!Directory.Exists(server_files))
+            Directory.CreateDirectory(server_files);
+
         while (true) {
             byte[] data = udp_server.Receive(ref endpoint);
             string message = Encoding.UTF8.GetString(data);
 
             Console.WriteLine(endpoint + " " + message);
+
+            string response;
+
+            if (message.StartsWith("admin"))
+            {
+                response = "FULL ACCESS (read/write/execute) ";
+            }
+            else if (message == "list")
+            {
+                var files = Directory.GetFiles(server_files);
+                response = string.Join(", ", files.Select(Path.GetFileName));
+            }
+            else
+            {
+                response = "READ ONLY ACCESS";
+            }
+
+            byte[] reply = Encoding.UTF8.GetBytes(response);
+            udp_server.Send(reply, reply.Length, endpoint);
         }
     }
 }
