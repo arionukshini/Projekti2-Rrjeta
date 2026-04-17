5202744311046002a0c42f5c89fee6084d70a50a
argjentatrepca-1 <argjenta.trepca@student.uni-pr.edu>
2026-04-17 21:29:01 +0200

    Configured UDP client dhe server endpoint

--- Klientet_ReadOnly/Program.cs
+++ Klientet_ReadOnly/Program.cs
@@ -4,3 +4,7 @@ using System.Net.Sockets;
 using System.Text;
 
 Console.WriteLine("Klienti Read-Only eshte gati");
+string server_ip = "127.0.0.1"; 
+int server_port = 4444;
+UdpClient client = new UdpClient();
+IPEndPoint server_endpoint = new IPEndPoint(IPAddress.Parse(server_ip), server_port);
